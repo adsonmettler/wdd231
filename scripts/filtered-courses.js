5a0bd0219bc316c0535cf1850e4da77049298788
Adson Mettler <adsonmettler@gmail.com>
2024-09-19 21:51:32 -0400

    updated 2024-09-19

--- scripts/filtered-courses.js
+++ scripts/filtered-courses.js
@@ -2,6 +2,17 @@
 // Author: Adson Mettler do Nascimento
 
 
+// Menu Navagation | Hamburguer menu button
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
 // COURSES OBJECTS
 
 document.addEventListener('DOMContentLoaded', function () {
